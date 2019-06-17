20344d47e427378601cd9ca39d464650700a1718
Valentinles <valentinvn@abv.bg>
2019-06-17 11:02:59 +0300

    services base structure added

---
+++ src/VehiclePortal.Services/Implementations/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VehiclePortal.Data;
+
+namespace VehiclePortal.Services.Implementations
+{
+    public class DataService
+    {
+        protected readonly VehiclePortalDbContext context;
+
+        public DataService(VehiclePortalDbContext context)
+        {
+            this.context = context;
+        }
+    }
+}

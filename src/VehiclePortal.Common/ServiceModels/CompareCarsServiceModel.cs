67b5f388de1f41fb2b06f3e3a899957f7613ca83
Valentinles <valentinvn@abv.bg>
2020-05-15 11:29:03 +0300

    CompareCarsServiceModel added

---
+++ src/VehiclePortal.Common/ServiceModels/CompareCarsServiceModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VehiclePortal.Models;
+
+namespace VehiclePortal.Common.ServiceModels
+{
+    public class CompareCarsServiceModel
+    {
+        public Car FirstCar { get; set; }
+
+        public Car SecondCar { get; set; }
+    }
+}

7f4ab376bef7e608900e40806ba3094539512f55
Valentinles <valentinvn@abv.bg>
2020-05-15 11:29:37 +0300

    CompareCarsViewModel added

---
+++ src/VehiclePortal.Common/ViewModels/CompareCarsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using VehiclePortal.Models;
+
+namespace VehiclePortal.Common.ViewModels
+{
+    public class CompareCarsViewModel
+    {
+        [Required]
+        public Car FirstCar { get; set; }
+
+        [Required]
+        public Car SecondCar { get; set; }
+    }
+}
